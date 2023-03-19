57852e0cf3f24d3fb7251511012bc65d3ab9d994
Ericka Bastias <erickabastias@Erickas-MacBook-Air.local>
2023-03-19 16:46:34 +0100

    added function to label data without adding indexes manually

--- src/label_data.py
+++ src/label_data.py
@@ -1,29 +1,13 @@
 import pandas as pd
 import os
+from index_gathering import create_label_dict
 
 while True:
-    # Define the start and end indices for each label
-    label_names = ["jumping", "walking", "jogging_in_place", "jumping_jacks", "sit_ups", "jumping_2"]
-    label_indexes = []
 
-    for label in label_names:
-        index_input = input("Input the start and end index separated by a space for %s: " % (label))
-        indexes_values = index_input.split(" ")
-
-        # Convert string to int values and create dict
-        indexes = {
-            "start": int(indexes_values[0]),
-            "end": int(indexes_values[1])
-        }
-
-        # Append dictionary to array of dict
-        label_indexes.append(indexes)
-
-
-    labels = {label_names[x]: label_indexes[x] for x in range(len(label_names))}
+    labels = create_label_dict()
 
     # Read the filename from terminal input
-    complete_filename = input("Enter the name of the CSV file: ")
+    complete_filename = input("Enter the name of the CSV file to label: ")
 
     if len(complete_filename) == 0 or complete_filename == None:
         print('No filename was given')
@@ -41,7 +25,7 @@ while True:
         end = index_range["end"]
         df.loc[start:end, "label"] = label
 
-    # Construct the new filename with the prefix "downsampled_"
+    # Construct the new filename with the prefix "labeled_"
     path, filename = os.path.split(complete_filename)
     print(path, filename)
     new_filepath = path + "/labeled_" + filename
